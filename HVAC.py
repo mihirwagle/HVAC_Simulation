d2e7837e93ac12824f9b6581be86d333c5acedfd
Mihir Wagle <mswagle@gmail.com>
2019-03-02 14:46:46 -0500

    Update logic
    Threading needs to be fixed.

--- HVAC.py
+++ HVAC.py
@@ -7,12 +7,13 @@ Created on Wed Feb 27 08:52:01 2019
 """
 
 import random
-
+import time
+import threading
 
 class HVAC:
     def __init__(self, idd, threshold):
         self.idd = idd
-        self.temp = random.randrange(-20,35)
+        self.temp = random.randrange(90.0,110.0)
         self.threshold = threshold
         self.parents = set()
         self.flag = 0
@@ -24,14 +25,24 @@ class HVAC:
         else:
             self.flag = 0
         while True:
-            self.temp += random.randrange(-1,1)
+            time.sleep(0.1)
+            self.temp += random.randrange(-1.0,1.0)
+            if self.temp < 80 or self.temp > 120:
+                self.temp = random.randrange(90.0,110.0)
+            print(str(self.getID()) + " : " + str(self.temp))
             if self.flag == 0:
                 if self.temp >= self.threshold:
                     # logic for update with locking
+                    for parent in self.parents:
+                        with parent.lock:
+                            parent.addErrant(self)
                     self.flag = 1
             else:
                 if self.temp < self.threshold:
                     # logic for update with locking
+                    for parent in self.parents:
+                        with parent.lock:
+                            parent.removeErrant(self)
                     self.flag = 0
         # this runs forever
         
@@ -44,6 +55,7 @@ class HVAC:
 
 class EdgeServer:
     def __init__(self, idd, childrenlist):
+        self.lock = threading.Lock()
         self.idd = idd
         self.parents = set()
         self.children = set()
@@ -65,18 +77,19 @@ class EdgeServer:
     def addErrant(self, node):
         self.listErrantNodes.add(node)
         for parent in self.parents:
-            # lock
-            parent.addErrant(node)
+            with parent.lock:
+                parent.addErrant(node)
 
     def removeErrant(self, node):
         self.listErrantNodes.discard(node)
         for parent in self.parents:
-            # lock
-            parent.removeErrant(node)
+            with parent.lock:
+                parent.removeErrant(node)
 
 
 class CentralHeatingSys:
     def __init__(self, idd, childrenlist):
+        self.lock = threading.Lock()
         self.idd = idd
         self.children = set()
         self.listErrantNodes = set()
@@ -92,9 +105,17 @@ class CentralHeatingSys:
     
     def addErrant(self, node):
         self.listErrantNodes.add(node)
+        if len(self.listErrantNodes) == 0:
+            print("")
+        for node in self.listErrantNodes:
+            print(node.getID())
     
     def removeErrant(self, node):
         self.listErrantNodes.discard(node)
+        if len(self.listErrantNodes) == 0:
+            print("")
+        for node in self.listErrantNodes:
+            print(node.getID())
 
 threshold = 100
 a = HVAC(1,100)
@@ -106,12 +127,26 @@ cd = EdgeServer(6,[c,d])
 abcd = CentralHeatingSys(7,[ab,cd])
 
 # Check for proper connections.
-x = abcd.children
-for element in x:
-    print(element.getID())
-x = ab.children
-for element in x:
-    print(element.getID())
-x = cd.children
-for element in x:
-    print(element.getID())
+#x = abcd.children
+#for element in x:
+#    print(element.getID())
+#x = ab.children
+#for element in x:
+#    print(element.getID())
+#x = cd.children
+#for element in x:
+#    print(element.getID())
+
+t1 = threading.Thread(target = a.run()).start()
+t1.daemon = True
+t2 = threading.Thread(target = b.run()).start()
+t2.daemon = True
+t3 = threading.Thread(target = c.run()).start()
+t3.daemon = True
+t4 = threading.Thread(target = d.run()).start()
+t4.daemon = True
+
+#t1.start()
+#t2.start()
+#t3.start()
+#t4.start()
